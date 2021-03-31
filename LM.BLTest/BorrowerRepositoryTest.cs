899a6e3a36faa0138a63882a928d74574591519e
Patryk Święickci <patswiecicki@gmail.com>
2021-03-31 22:06:01 +0200

    Add BorrowerRepository Unit Test

---
+++ LM.BLTest/BorrowerRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LM.BL;
+
+namespace LM.BLTest
+{
+    [TestClass]
+    public class BorrowerRepositoryTest
+    {
+        [TestMethod]
+        public void RetriveValid()
+        {
+            var borrowerRepository = new BorrowerRepository();
+            var expected = new Borrower(1)
+            {
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                EmailAddress = "JaneKowalski@poczta.pl"
+            };
+
+            var actual = borrowerRepository.Retrive(1);
+
+            Assert.AreEqual(expected.BorrowerId, actual.BorrowerId);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+        }
+    }
+}
