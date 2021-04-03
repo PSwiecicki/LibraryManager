75666bab9170ada6ff7cbc408dda50611a9914da
Patryk Święickci <patswiecicki@gmail.com>
2021-04-03 09:29:52 +0200

    Add unit tests for Borrower EmailAddress setter

--- LM.BLTest/BorrowerTest.cs
+++ LM.BLTest/BorrowerTest.cs
@@ -38,5 +38,81 @@ namespace LM.BLTest
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void EmailAddressWithoutAt()
+        {
+            Borrower borrower = new() { EmailAddress = "jankowalskigmail.com" };
+            string expected = null;
+
+            string actual = borrower.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmailAddressWithoutLocalPart()
+        {
+            Borrower borrower = new() { EmailAddress = "@gmail.com" };
+            string expected = null;
+
+            string actual = borrower.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmailAddressWithoutDot()
+        {
+            Borrower borrower = new() { EmailAddress = "jankowalski@gmailcom" };
+            string expected = null;
+
+            string actual = borrower.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EmailAddressWithoutFirstDomainPart()
+        {
+            Borrower borrower = new() { EmailAddress = "jankowlaki@.com" };
+            Borrower borrower2 = new() { EmailAddress = "jankowlaki@.gmail.com" };
+            string expected = null;
+
+            string actual = borrower.EmailAddress;
+            string actual2 = borrower2.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual2);
+        }
+
+        [TestMethod]
+        public void EmailAddressWithoutLastDomainPart()
+        {
+            Borrower borrower = new() { EmailAddress = "jankowlaki@gmail" };
+            Borrower borrower2 = new() { EmailAddress = "jankowlaki@gmail.com." };
+            string expected = null;
+
+            string actual = borrower.EmailAddress;
+            string actual2 = borrower2.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual2);
+        }
+
+        [TestMethod]
+        public void EmailAddressValid()
+        {
+            Borrower borrower = new() { EmailAddress = "jankowalski@gmail.com" };
+            Borrower borrower2 = new() { EmailAddress = "jankowalski@uczelnia.edu.pl" };
+            string expected = "jankowalski@gmail.com";
+            string expected2 = "jankowalski@uczelnia.edu.pl";
+
+            string actual = borrower.EmailAddress;
+            string actual2 = borrower2.EmailAddress;
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected2, actual2);
+        }
     }
 }
